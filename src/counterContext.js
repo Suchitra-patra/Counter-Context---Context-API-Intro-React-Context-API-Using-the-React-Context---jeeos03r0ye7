85092e2bd651d90baead4b95d64020759dd5ee23
Suchitra-patra <suchitra3576@gmail.com>
2023-02-25 23:03:02 +0530

    first

--- src/counterContext.js
+++ src/counterContext.js
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 
 
@@ -8,8 +8,11 @@ const counterContext = createContext()
 
 
 const CounterStateContext = (props) => {
+    const [count, setCount] = useState(0);
+   
 
     return (
+        
         <div id="counter-context">
             <counterContext.Provider value={{ count, setCount }}>
                 {props.children}
